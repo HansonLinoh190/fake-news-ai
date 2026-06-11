1522b89b5f8992b1e14e6aec4affc33b31948556
Hanson Linoh <hanson.linoh@binus.ac.id>
2026-06-11 23:32:59 +0700

    Add files via upload

--- index.py
+++ index.py
@@ -1,9 +1,7 @@
 import os
 import sys
 
-# Resolve project path and imports
 base_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(base_dir, "src"))
-sys.path.append(os.path.join(base_dir, "app"))
 
 from app.app import app
